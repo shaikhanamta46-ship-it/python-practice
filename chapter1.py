96ba4132ebf148ce9f1f21748f0e876b6fa8b61c
Anamta Shaikh <shaikhanamta46@gmail.com>
2026-05-01 20:35:39 +0530

    initial commit - added python practice file
    This file includes my python practice code where. I am learning basic concepts like variables, datatypes and simple programs.

---
+++ chapter1.py
@@ -0,0 +1,20 @@
+# take input form the user
+name = input("enter name:")
+age = int(input("enter age:"))
+marks = float(input("enter marks:"))
+print(name,age,marks)
+
+#practice question1
+number1 = int(input("enter number1:"))
+number2 = int(input("enter number2:"))
+sum = number1 + number2
+print(sum)
+#2 practise question
+a = float(input("enter first:"))
+b = float(input("enter second:"))
+print("avg=",(a+b)/2) 
+
+#4 practise question
+a = int(input("enter first:"))
+b = int(input("enter second:"))
+print(a>=b)
